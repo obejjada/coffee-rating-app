a9f714e4b2bd92e28c9c16159523d2ea397890e1
Omar <83602375+obejjada@users.noreply.github.com>
2023-01-23 06:14:45 +0000

    Create method for creating database
    Created a shell method for creating the application database

--- Code/databse_actions.py
+++ Code/databse_actions.py
@@ -1,3 +1,8 @@
 """Module contains classes and methods to manipulate the application database"""
 class DatabaseCreation():
     """DatabaseCreation class contains methods to create the database for the application"""
+    def create_database(self):
+        """
+        Method to create the database if one does not exist
+        """
+        pass
