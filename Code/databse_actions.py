ddd66bd67abe251f135253bdee404d4e2e636f74
Omar <83602375+obejjada@users.noreply.github.com>
2023-01-23 06:10:27 +0000

    Created class for creating database
    Created a class which will be used to house the methods for creating the application database

--- Code/databse_actions.py
+++ Code/databse_actions.py
@@ -1,1 +1,3 @@
 """Module contains classes and methods to manipulate the application database"""
+class DatabaseCreation():
+    """DatabaseCreation class contains methods to create the database for the application"""
