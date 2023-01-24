faa6c9ca0d6a58a50eafe58c8a70a2f5da9d10af
Omar <83602375+obejjada@users.noreply.github.com>
2023-01-24 05:19:03 +0000

    created test for verifying database is created
    created a unit test to verify the application database has been created.

--- Test/test_database_creation.py
+++ Test/test_database_creation.py
@@ -1,3 +1,13 @@
 """ Test Module used to hold the unit tests that will verify the methods related to the database"""
+import os
+from Code.databse_actions import DatabaseCreation
+
+DATABASE_PATH = os.path.dirname(os.path.abspath(__file__)) + r'\Database\coffee-rating-app-database.db'
+
 class TestDatabaseCreation():
     """Test class to veirfy the application database is created correctly"""
+    def test_create_database_file(self):
+        """ Method to verify if the application database has been created"""
+        databasecreation =DatabaseCreation()
+        databasecreation.create_database()
+        assert os.path.exists(DATABASE_PATH)
