f0655c316edad6095516fe726271b5eaf5a8c891
Omar <83602375+obejjada@users.noreply.github.com>
2023-01-24 05:40:23 +0000

    update test to provide input parameter
    update the test to call the create_database method with an input parameter for the path of where the database will be created. CURRENT_PATH global variable created to facilitate that. DATABASE_PATH global variable renamed to TEST_DATABASE_PATH to reflect that the path is the location of the test database

--- Test/test_database_creation.py
+++ Test/test_database_creation.py
@@ -1,13 +1,13 @@
 """ Test Module used to hold the unit tests that will verify the methods related to the database"""
 import os
 from Code.databse_actions import DatabaseCreation
-
-DATABASE_PATH = os.path.dirname(os.path.abspath(__file__)) + r'\Database\coffee-rating-app-database.db'
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__)) 
+TEST_DATABASE_PATH = CURRENT_PATH + r'\Database\coffee-rating-app-database.db'
 
 class TestDatabaseCreation():
     """Test class to veirfy the application database is created correctly"""
     def test_create_database_file(self):
         """ Method to verify if the application database has been created"""
-        databasecreation =DatabaseCreation()
-        databasecreation.create_database()
-        assert os.path.exists(DATABASE_PATH)
+        databasecreation = DatabaseCreation()
+        databasecreation.create_database(CURRENT_PATH)
+        assert os.path.exists(TEST_DATABASE_PATH)
