a4bfd74ece22789024ca86c5dad22506715d1404
Omar <83602375+obejjada@users.noreply.github.com>
2023-01-24 03:13:04 +0000

    Create test module for database creation
    Create a skeleton module and class for developing test methods that verify database creation methods

---
+++ Test/test_database_creation.py
@@ -0,0 +1,3 @@
+""" Test Module used to hold the unit tests that will verify the methods related to the database"""
+class TestDatabaseCreation():
+    """Test class to veirfy the application database is created correctly"""
