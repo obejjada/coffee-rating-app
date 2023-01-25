903b891b1567d9e003607faf481bbb689ff8b875
Omar <83602375+obejjada@users.noreply.github.com>
2023-01-25 03:24:49 +0000

    update test_create_database_file test
    test_create_database_file  updated as re-running the test created an OSError Directory not empty. shutil.rmtree is needed to recursively delete directory.

--- Test/test_database_creation.py
+++ Test/test_database_creation.py
@@ -1,5 +1,6 @@
 """ Test Module used to hold the unit tests that will verify the methods related to the database"""
 import os
+import shutil
 from Code.databse_actions import DatabaseCreation
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 TEST_DATABASE_PATH = CURRENT_PATH + r'\Database\coffee-rating-app-database.db'
@@ -18,7 +19,7 @@ class TestDatabaseCreation():
     def test_create_database_file(self):
         """ Method to verify if the application database has been created"""
         if os.path.exists(CURRENT_PATH + r'\Database'):
-            os.rmdir(CURRENT_PATH + r'\Database')
+            shutil.rmtree(CURRENT_PATH + r'\Database')
         self.databasecreation.create_database(CURRENT_PATH)
         assert os.path.exists(TEST_DATABASE_PATH)
     def test_create_table(self):
