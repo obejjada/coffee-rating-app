9badcdc04f311d0cf63c656aebdbc5e687e6033f
Omar <83602375+obejjada@users.noreply.github.com>
2023-01-25 02:45:29 +0000

    create_database method created
    Coded create_database method. The method checks if the folder where the database will live exists because sqlite3.connect() can not be used to create a folder. The method then creates the application database.

--- Code/databse_actions.py
+++ Code/databse_actions.py
@@ -1,8 +1,11 @@
 """Module contains classes and methods to manipulate the application database"""
+import sqlite3
+import os
 class DatabaseCreation():
     """DatabaseCreation class contains methods to create the database for the application"""
-    def create_database(self):
-        """
-        Method to create the database if one does not exist
-        """
-        pass
+    def create_database(self, database_path):
+        """Method to create the database if one does not exist"""
+        if not os.path.exists(database_path + r'\Database'):
+            os.makedirs(database_path + r'\Database')
+            connection = sqlite3.connect(database_path + r'\Database\coffee-rating-app-database.db')
+            connection.close()
