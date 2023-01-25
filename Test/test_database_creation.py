81df34a89bffab33bd80bda158368759a5c9e4f7
Omar <83602375+obejjada@users.noreply.github.com>
2023-01-25 12:36:05 +0000

    write test for rating table
    created a unit test for verifying the table within the database has been created correctly with the following:
    Table name
    Column names
    Column Types

--- Test/test_database_creation.py
+++ Test/test_database_creation.py
@@ -1,6 +1,7 @@
 """ Test Module used to hold the unit tests that will verify the methods related to the database"""
 import os
 import shutil
+import sqlite3
 from Code.databse_actions import DatabaseCreation
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 TEST_DATABASE_PATH = CURRENT_PATH + r'\Database\coffee-rating-app-database.db'
@@ -23,5 +24,26 @@ class TestDatabaseCreation():
         self.databasecreation.create_database(CURRENT_PATH)
         assert os.path.exists(TEST_DATABASE_PATH)
     def test_create_table(self):
-        """ Method to verify a table is created in the database with the correct title and column headings"""
-        pass
+        """ Method to verify a table is created in the database with the correct title and column headings
+        Title: Rating Table
+        Column 1: Date (Text)
+        Column 2: Coffee Shop Name (Text)
+        Column 3: Coffee Beverage (Text)
+        Column 4: Rating (Real)
+        """
+        self.databasecreation.create_table(TEST_DATABASE_PATH)
+        connection = sqlite3.connect(TEST_DATABASE_PATH)
+        cursor = connection.cursor()
+
+        assert len(cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Rating Table'").fetchall()) == 1
+
+        column_data = cursor.execute("PRAGMA table_info(Rating Table)").fetchall()
+        
+        assert column_data[0][1] == "Date"
+        assert column_data[0][2] == "Text"
+        assert column_data[1][1] == "Coffee Shop Name"
+        assert column_data[1][2] == "Text"
+        assert column_data[2][1] == "Coffee Beverage"
+        assert column_data[2][2] == "Text"
+        assert column_data[3][1] == "Rating"
+        assert column_data[3][2] == "Text"
