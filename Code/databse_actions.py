6647c2757cfe9f1350f0149dea050207ea56db39
Omar <83602375+obejjada@users.noreply.github.com>
2023-01-28 10:06:49 +0000

    Code creating database method
    method coded for creating the rating table within the application database. "CREATE TABLE IF NOT EXISTS" used so an error is not raised if the method is called when a table with the same name is called.

--- Code/databse_actions.py
+++ Code/databse_actions.py
@@ -11,4 +11,13 @@ class DatabaseCreation():
             connection.close()
     def create_table(self, database_path):
         """Mthod to create a table within the database with the following attributes"""
-        pass
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()#
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Rating_Table(
+        Date text,
+        Coffee_Shop_Name text,
+        Coffee_Beverage text,
+        Rating real)""")
+        connection.commit()
+        connection.close()
