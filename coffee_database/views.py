d2578d1b7fd928da6812058a372db2553d2e0aa9
Omar <83602375+obejjada@users.noreply.github.com>
2025-01-21 15:55:14 +0000

    Update the database_return view to use the api/all endpoint to retrieve the contents of the database

--- coffee_database/views.py
+++ coffee_database/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
 from django.utils import timezone
 from django.shortcuts import redirect
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import CoffeeDatabase
 from .searializer import CoffeeSerializer
+import requests
 
 
 @api_view(['GET'])
@@ -25,20 +24,13 @@ def home(request):
 
 
 def database_return(request):
-    '''Method to return the entire entries in the database'''
-    database_entries = CoffeeDatabase.objects.values()
-    template = loader.get_template("coffee_database/all-records.html")
-    complete_entries = []
-    for index in range(len(database_entries)):
-        context = {"Date": database_entries[index].get("date_time"),
-                   "coffee_shop": database_entries[index].get("coffee_shop"),
-                   "coffee_beverage": database_entries[index].get(
-                   "coffee_beverage"),
-                   "rating": database_entries[index].get("rating"),
-                   "id": database_entries[index].get("id")}
-        complete_entries.append(context)
-    coffee_dict = {"all_records": complete_entries}
-    return HttpResponse(template.render(coffee_dict, request))
+    '''Method to return the entire entries in the database using
+       get_all_coffee_drinks API endpoint'''
+    url = "http://127.0.0.1:8000/coffee_database/api/all"
+    response = requests.get(url)
+    api_return = response.json()
+    template = "coffee_database/all-records.html"
+    return render(request, template, {"api_returns": api_return})
 
 
 def submit_form(request):
