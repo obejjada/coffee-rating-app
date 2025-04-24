f824a8ee155e756f90e10954f10d66144bee8f31
Omar <83602375+obejjada@users.noreply.github.com>
2025-04-25 00:17:59 +0100

    Create frontend page that fetches the api call for all the coffee drinks in the database and render them on the page"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      coffeeDrinks:[],
+      activeDrink:{
+        id:null,
+        coffeeShop:'',
+        coffeeBeverage:'',
+        rating:null
+      }
+    }
+    this.fetchDrinks = this.fetchDrinks.bind(this)
+  };
+
+  componentWillMount(){
+    this.fetchDrinks()
+  }
+
+  fetchDrinks(){
+    console.log('Fetching')
+
+    fetch('http://127.0.0.1:8000/coffee_database/api/coffee_drinks/all/')
+    .then(response => response.json())
+    .then(data => 
+      this.setState({
+        coffeeDrinks:data
+      })
+      )
+ }
+  
+  render(){
+    var drinks = this.state.coffeeDrinks
+    return(
+    <div className="container">
+      <div id="coffee-drinks-container">
+          {drinks.map(function(drink, index){
+            return(
+              <div key={index} className='listwrapper flex-wrapper'>
+              <div style={{flex:1}}>
+                <span>{drink.coffee_beverage}</span>
+              </div>
+              <div style={{flex:2}} >
+                <span>{drink.coffee_shop}</span>
+              </div>
+              <div style={{flex:1}}>
+                <span>{drink.rating}</span>
+              </div>
+              </div>)
+          })}
+          </div>
+        </div>
+    )
+  }
 }
 
 export default App;
