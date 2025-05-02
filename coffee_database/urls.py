4a922f262ffccb0a00ec645247bf9848fee19429
Omar <83602375+obejjada@users.noreply.github.com>
2025-05-02 23:15:26 +0100

    Update the code and workspace to clean old code. As the development is using react frontend, remove the urls and html templates

--- coffee_database/urls.py
+++ coffee_database/urls.py
@@ -3,11 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    #path("home", views.home, name="home"),
-    #path("All-Coffee-Drinks", views.database_return, name="database_return"),
-    #path("Coffee-Drink-Entry-Form", views.submit_form, name='submit_form'),
-    #path("coffee-entered", views.submit_record, name='submit_record'),
-    #path("delete-entry", views.delete_entry, name='delete_entry'),
     path("api/coffee_drinks/all/", views.get_all_coffee_drinks,
          name='get_all_coffee_drinks'),
     path("api/coffee_drinks/create/", views.create_new_entry,
--- coffee_database/views.py
+++ coffee_database/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render
-from django.utils import timezone
-from django.shortcuts import redirect
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from .models import CoffeeDatabase
 from .searializer import CoffeeSerializer
-import requests
 
 
 @api_view(['GET'])
@@ -27,47 +23,3 @@ def create_new_entry(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#def home(request):
-#    '''Method to return the use to the home page'''
-#    template = "coffee_database/home.html"
-#    return render(request, template)
-#
-#
-#def database_return(request):
-#    '''Method to return the entire entries in the database using
-#       get_all_coffee_drinks API endpoint'''
-#    url = "http://127.0.0.1:8000/coffee_database/api/coffee_drinks/all/"
-#    response = requests.get(url)
-#    if response.status_code == 200:
-#        api_return = response.json()
-#    else:
-#        api_return = []
-#    template = "coffee_database/All-Coffee-Drinks.html"
-#    return render(request, template, {"api_return": api_return})
-#
-#
-#def submit_form(request):
-#    '''Method that displays the entry form to the database'''
-#    template = "coffee_database/Coffee-Drink-Entry-Form.html"
-#    return render(request, template)
-#
-#
-#def submit_record(request):
-#    '''Method to add record to the database'''
-#    entry = CoffeeDatabase(date_time=timezone.now(),
-#                           coffee_shop=request.POST['coffee_shop'].title(),
-#                           coffee_beverage=request.POST['coffee_beverage'].title(),
-#                           rating=request.POST['rating'])
-#    entry.save()
-#    response = redirect('/coffee_database/All-Coffee-Drinks')
-#    return response
-#
-#
-#def delete_entry(request):
-#    '''Method to delete selected records from the database'''
-#    for checked in request.POST.getlist('checkbox'):
-#        entry = CoffeeDatabase.objects.get(id=checked)
-#        entry.delete()
-#    response = redirect('/coffee_database/All-Coffee-Drinks')
-#    return response
