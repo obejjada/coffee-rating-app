235525ba20007930c371b0a06f9dbce95972e46c
Omar <83602375+obejjada@users.noreply.github.com>
2025-05-22 17:35:45 +0100

    Create seralizers to read info in JSON format for API requests

--- coffee_database/searializer.py
+++ coffee_database/searializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CoffeeDatabase, CoffeeBeanDatabase
+from .models import CoffeeDatabase, CoffeeBeanDatabase, CoffeeOrigin, FlavourNote
 
 
 class CoffeeSerializer(serializers.ModelSerializer):
@@ -13,13 +13,32 @@ class CoffeeSerializer(serializers.ModelSerializer):
                   'coffee_beverage', 'rating']
 
 
+class CoffeeOriginSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CoffeeOrigin
+        fields = ['id', 'country', 'country_name']
+
+
 class CoffeeBeanSerializer(serializers.ModelSerializer):
     formatted_datetime = serializers.DateTimeField(source='date_time',
                                                    format='%Y-%m-%d %H:%M:%S',
                                                    read_only=True)
 
+    country_origin = serializers.SlugRelatedField(
+        many=True,
+        queryset=CoffeeOrigin.objects.all(),
+        slug_field="country_name",
+    )
+
+    flavour_notes = serializers.SlugRelatedField(
+        many=True,
+        queryset=FlavourNote.objects.all(),
+        slug_field="flavour_notes"
+    )
+
     class Meta:
         model = CoffeeBeanDatabase
-        fields = ['id', 'formatted_datetime', 'roast_name', 'coffee_roaster'
-                  'is_single_origin', 'country_roaster', 'country_origin'
+        fields = ['id', 'formatted_datetime', 'roast_name', 'coffee_roaster',
+                  'is_signle_origin', 'country_roaster', 'country_origin',
                   'region', 'flavour_notes', 'process', 'rating', 'comment']
